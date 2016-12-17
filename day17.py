1ae8d430f81b3628ac45af9ff738f4b1838f12ad
Ben McKeown <mckeowbc@gmail.com>
2016-12-17 09:54:25 -0500

    A little prettier.

--- day17.py
+++ day17.py
@@ -4,7 +4,7 @@ import sys
 from hashlib import md5
 import re
 
-grid_size = (4,4)
+endloc = (3,3)
 
 directions = ('U','D','L','R')
 
@@ -25,41 +25,33 @@ def getpos(d,pos):
     else:
         raise ValueError, 'Invalid direction: %s' % d
         
-    if newpos[0] < 0 or  newpos[1] < 0 or \
-       newpos[0] >= grid_size[0] or newpos[1] >= grid_size[1]:
+    if any(l < 0 for l in newpos) or \
+       any(l > endloc[i] for i,l in enumerate(newpos)):
         raise IndexError,repr(pos)
     return newpos
 
-def walk(salt,path,loc):
+def walk(salt,path,loc,endloc):
     #print >>sys.stderr,'%s %s' % (path,loc)
-    if loc == (3,3):
+    if loc == endloc:
         return [path]
         
-    prefix = md5(salt + path).hexdigest()[0:4]
-    dirs = []
-    for l in prefix:
-        if open_re.match(l):
-            dirs.append(True)
-        elif closed_re.match(l):
-            dirs.append(False)
-        else:
-            raise RunTimeError,'%s%s:%s' % (salt,path,prefix)
-    
+    dirs = [True if open_re.match(l) else False
+                for l in md5(salt + path).hexdigest()[0:4]]
     sols = []
     for i,d in enumerate(dirs):
         if d:
-            try:
-                direct = directions[i]
+            direct = directions[i]
+            try:  
                 newloc = getpos(direct,loc)
-                sols += walk(salt,path+direct,newloc)
+                sols += walk(salt,path+direct,newloc,endloc)
             except IndexError:
                 pass
     return sols
                 
 salt = sys.argv[1]
-solutions = walk(salt,'',(0,0))
+solutions = walk(salt,'',(0,0),endloc)
 
 for sol in sorted(solutions,key=len):
-    print sol,len(sol)
+    print len(sol),sol
     
 
